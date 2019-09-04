2b874194e9a91864ccc984d62496b5e524ca7115
Stacy Johns <stacykjohns@icloud.com>
2019-09-04 19:16:53 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,28 @@
+class Author
+  attr_accessor :name
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+    @@post_count += 1
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
+
+  def self.post_count
+    @@post_count
+  end
+end
